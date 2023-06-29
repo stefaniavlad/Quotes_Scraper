ad812dea0904d02c8e26a7a2cb30536a360143fd
stefaniavlad <vladstefania98@gmail.com>
2023-06-29 16:26:32 +0300

    hi

---
+++ Quote_Scraper/Quote_Scraper/spiders/my_script.py
@@ -0,0 +1,20 @@
+import scrapy
+
+# inherit the class
+class myScraper(scrapy.Spider):
+    # name for the scraper
+    name = 'mycrawler'
+    start_urls = ['https://quotes.toscrape.com/']
+
+# define module/function
+    def parse(self, response):
+        authors = response.css("small.author::text").extract()
+        quotes = response.css("span.text::text").extract()
+        tags = response.css("a.tag::text").extract()
+        tag_link = response.css("a.tag::attr(href)").extract()
+        yield {
+            "List_Authors": authors,
+            "List_Quotes": quotes,
+            "List_Tags": tags,
+            "List_Tag_Links": tag_link
+        }
